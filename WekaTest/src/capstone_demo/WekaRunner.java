6cc74ac828f1a0be3f64c0aceffcb7569864f344
MattQuintana <mq56@nau.edu>
2018-02-20 22:03:13 -0700

    Implemented Frequency Counter
    Created the Frequency counter method.
    Method Summary: Takes in a group of files and performs the Weka feature
    selection on them. Counts which features appear the most over each file
    analysis. Returns a sorted list of attributes and their count.

--- WekaTest/src/capstone_demo/WekaRunner.java
+++ WekaTest/src/capstone_demo/WekaRunner.java
@@ -5,6 +5,8 @@ import java.lang.*;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import weka.attributeSelection.AttributeSelection;
@@ -25,9 +27,10 @@ import weka.core.converters.CSVLoader;
 
 public class WekaRunner {
 	
-
+	
+	// Runs the Weka attribute selection and returns a set of indices of important attributes 
 	protected static int[] useLowLevel(Instances data) throws Exception {
-	    System.out.println("\n2. Low-level");
+	    //System.out.println("\n2. Low-level");
 	    AttributeSelection attsel = new AttributeSelection();
 	    CfsSubsetEval eval = new CfsSubsetEval();
 	    GreedyStepwise search = new GreedyStepwise();
@@ -36,18 +39,22 @@ public class WekaRunner {
 	    attsel.setSearch(search);
 	    attsel.SelectAttributes(data);
 	    int[] indices = attsel.selectedAttributes();
-	    System.out.println("selected attribute indices (starting with 0):\n" + Utils.arrayToString(indices) + "\n");
+	    //System.out.println("selected attribute indices (starting with 0):\n" + Utils.arrayToString(indices) + "\n");
 	    return indices;
 	}
 	
-	// Takes in a list of files and 
+	// Takes in a list of files, performs a feature analysis, and counts the 
+	// frequency by which attributes are chosen.
+	// Returns a list that contains the different attribute indices and their frequency count 
 	public static List<Integer[]> frequencyCounter(File[] analysis_files) throws Exception
 	{
+		// Create a list to hold the attribute indices and it's count  
 		List<Integer[]> best_attr = new ArrayList<Integer[]>();
 		
 		// Run the analysis on several files 
 		for (int i = 0; i < analysis_files.length; i++)
 		{
+			
 			// Get the current file to analyze
 			// Convert it to a instances class
 			File current_file = analysis_files[i];
@@ -56,55 +63,76 @@ public class WekaRunner {
 			
 			// Pass the data into the analysis
 			int[] weka_attr = useLowLevel(file_data);
-			// Show what attributes were chosen
-			// System.out.println(Utils.arrayToString(weka_attr));
 			
 			// Add in the attribute index into the array
 			// This is where we would do some counting and sorting 
 			for (int j = 0; j < weka_attr.length; j++) 
 			{
-				System.out.println(weka_attr[j]);
+				
+				// Boolean to check if it's seen an attribute before
+				Boolean found_previous = false; 
+				
 				// Check every entry in the already seen attributes
 				for (Integer[] entry : best_attr)
 				{
 					// Check if it's been seen before
-					System.out.println("In for loop");
-					System.out.println(entry);
 					if (entry[0] == weka_attr[j])
 					{
 						// Increment the count
 						entry[1] += 1;
-					}
-					// If not, add into array. 
-					else
-					{
-						// Make a new entry
-						Integer[] new_entry = {weka_attr[j], 1};
-						best_attr.add(new_entry);
+						found_previous = true;
+						break;
 					}
 				}
+				// If it searched through the entire list and didn't find the entry
+				if (found_previous == false)
+				{
+					// Make a new entry
+					Integer[] new_entry = {weka_attr[j], 1};
+					best_attr.add(new_entry);
+				}								
 			}
 		}
 		
+		// Build a comparator to sort the list by count
+		Comparator<Integer[]> sorter = new Comparator<Integer[]>()
+		{
+			@Override
+			public int compare(Integer[] o1, Integer[] o2) {
+	            int res = o2[1].compareTo(o1[1]);
+	            return res;
+	        }
+		};
+		
+		// Sort the list by count, descending order
+		best_attr.sort(sorter);
+		
+		// Print out each attribute and its count. 
+		/*
 		for (Integer[] row: best_attr)
 		{
-			System.out.println(row[0].toString());
-		}
-		
-		System.out.println(best_attr.toString());
-		
+			System.out.println(row[0].toString() + " " + row[1].toString());
+		}*/
 		return best_attr;
 	}
+	
 	
 	public static void main(String[] args) throws Exception
 	{
 		File carole_arff = new File("C:/Users/Matt Q/eclipse-workspace/AuToBI/analysis_results/FCAE_0026Carole/phrase_accent_classifier/FCAE_0026Carole_phac_analysis.arff");
 		File elisabeth_arff = new File("C:/Users/Matt Q/eclipse-workspace/AuToBI/analysis_results/FCAE_0028Elisabeth/phrase_accent_classifier/FCAE_0028Elisabeth_phac_analysis.arff");
+		File elodie_arff = new File("C:/Users/Matt Q/eclipse-workspace/AuToBI/analysis_results/FCAE_0029Elodie/phrase_accent_classifier/FCAE_0029Elodie_phac_analysis.arff");
 		File ryoko_arff = new File("C:/Users/Matt Q/eclipse-workspace/AuToBI/analysis_results/FCAE_0029Ryoko/phrase_accent_classifier/csv_result-FCAE_0029Ryoko_phac_analysis.csv");
+		File helena_arff = new File("C:/Users/Matt Q/eclipse-workspace/AuToBI/analysis_results/FCAE_0030Helena/phrase_accent_classifier/FCAE_0030Helena_phac_analysis.arff");
+		File ebba_arff = new File("C:\\Users\\Matt Q\\eclipse-workspace\\AuToBI\\analysis_results\\FCAE_0031Ebba\\phrase_accent_classifier\\FCAE_0031Ebba_phac_analysis.arff");
 		
-		File[] files = {carole_arff, elisabeth_arff, ryoko_arff};
+		File[] files = {carole_arff, elisabeth_arff, ryoko_arff, elodie_arff, helena_arff, ebba_arff};
 		
-		frequencyCounter(files);	
+		List<Integer[]> attr_count = frequencyCounter(files);
+		for (int i = 0; i < 10; i++)
+		{
+			System.out.println(attr_count.get(i)[0].toString() + " " + attr_count.get(i)[1].toString());
+		}
 	}
 }
 
